cf0f213e0154bac512b4d8263b986294ec0f34c5
Athina <athina.kantis@gmail.com>
2025-01-30 17:56:23 +0200

    update PMA to add stored projects to the correct list

--- src/models/project-model.ts
+++ src/models/project-model.ts
@@ -5,11 +5,11 @@ export enum ProjectStatus {
 
 export class Project {
     constructor(
-        public id: string,
         public title: string,
         public description: string,
         public people: number,
         public status: ProjectStatus,
-        public comment: string
+        public comment: string,
+        public id: string,
     ) {}
 }
--- src/state/project-state.ts
+++ src/state/project-state.ts
@@ -4,79 +4,106 @@ import { Project, ProjectStatus } from '../models/project-model';
 type Listener<T> = (items: T[]) => void;
 
 export class State<T> {
-    protected listeners: Listener<T>[] = [];
+  protected listeners: Listener<T>[] = [];
 
-    addListener(listenerFn: Listener<T>) {
-        this.listeners.push(listenerFn);
-    }
+  addListener(listenerFn: Listener<T>) {
+    this.listeners.push(listenerFn);
+  }
 }
 
 export class ProjectState extends State<Project> {
-    private projects: Project[] = [];
-    private static instance: ProjectState;
+  private projects: Project[] = [];
+  private static instance: ProjectState;
 
-    private constructor() {
-        super();
+  private constructor() {
+    super();
+  }
+
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new ProjectState();
+    return this.instance;
+  }
+
+  addProject(
+    title: string,
+    description: string,
+    numOfPeople: number,
+    status: ProjectStatus,
+    comment: string,
+    id: string = Math.random().toString()
+  ) {
+    const newProject = new Project(
+      title,
+      description,
+      numOfPeople,
+      status,
+      comment,
+      id
+    );
+    this.projects.push(newProject);
+    localStorage.setItem('projects', JSON.stringify(this.projects));
+    this.updateListeners();
+  }
+
+  moveProject(
+    projectId: string,
+    newStatus: ProjectStatus = ProjectStatus.Finished
+  ) {
+    const project = this.projects.find((prj) => prj.id === projectId);
+    if (project && project.status !== newStatus) {
+      project.status = newStatus;
+      this.updateListeners();
     }
 
-    static getInstance() {
-        if (this.instance) {
-            return this.instance;
-        }
-        this.instance = new ProjectState();
-        return this.instance;
-    }
+    const storageData = localStorage.getItem('projects');
+    const storage = storageData ? JSON.parse(storageData) : [];
+    const storageProject = storage.find(
+      (project: Project) => project.id === projectId
+    );
+    storageProject.status = newStatus;
+    localStorage.setItem(
+      'projects',
+      JSON.stringify(Object.assign(storage, storageProject))
+    );
+  }
 
-    addProject(
-        title: string,
-        description: string,
-        numOfPeople: number,
-        comment: string
-    ) {
-        const newProject = new Project(
-            Math.random().toString(),
-            title,
-            description,
-            numOfPeople,
-            ProjectStatus.Active,
-            comment
-        );
-        this.projects.push(newProject);
-        localStorage.setItem('projects', JSON.stringify(this.projects));
-        this.updateListeners();
+  private updateListeners() {
+    for (const listenerFn of this.listeners) {
+      listenerFn(this.projects.slice());
     }
+  }
 
-    moveProject(projectId: string, newStatus: ProjectStatus) {
-        const project = this.projects.find((prj) => prj.id === projectId);
-        if (project && project.status !== newStatus) {
-            project.status = newStatus;
-            this.updateListeners();
-        }
-
-        const storageData = localStorage.getItem('projects');
-        const storage = storageData ? JSON.parse(storageData) : [];
-    }
-
-    private updateListeners() {
-        for (const listenerFn of this.listeners) {
-            listenerFn(this.projects.slice());
-        }
-    }
+  clearProjects() {
+      console.log('clearing')
+    this.projects = [];
+    localStorage.removeItem('projects');
+    console.log(this.projects)
+  }
 }
 
 export const projectState = ProjectState.getInstance();
 
+/* Load projects when DOM content has loaded */
 document.addEventListener('DOMContentLoaded', () => loadProjects());
 
 const loadProjects = () => {
-    const storageData = localStorage.getItem('projects');
-    const storage = storageData ? JSON.parse(storageData) : [];
-    for (const project of storage) {
-        projectState.addProject(
-            project.title,
-            project.description,
-            +project.people,
-            project.comment
-        );
+  const storageData = localStorage.getItem('projects');
+  const storage = storageData ? JSON.parse(storageData) : [];
+  for (const project of storage) {
+    projectState.addProject(
+      project.title,
+      project.description,
+      project.people,
+      project.status,
+      project.comment,
+      project.id
+    );
+    if (project.status === ProjectStatus.Finished) {
+      projectState.moveProject(project.id, ProjectStatus.Finished);
     }
+  }
 };
+
